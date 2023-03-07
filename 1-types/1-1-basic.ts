a9144ac4092710b90869bb5d478b91e1f52b6957
park songhee <songhee6392@gmail.com>
2023-03-08 02:43:57 +0900

    2.5 기본 타입 정리 3 (unknown, any, void, never, object)

--- 1-types/1-1-basic.ts
+++ 1-types/1-1-basic.ts
@@ -24,4 +24,35 @@
     //null : 값이 비어있는 상태(값이 비어있다고 결정)
     let person: null; //💩 이렇게는 쓰지않는다.
     let person2: string | null;
+
+    //unknown : 어떤 종류인지 알수 없는 타입인 경우 💩 가능하면 쓰지않는 것이 좋다.
+    let notSure: unknown = 0;
+    notSure = "he"; //문자열 할당 가능
+    notSure = true; //boolean 할당 가능
+
+    //any : 어떤 것이든 담을 수 있는 타입
+    let anything: any = 0;
+    anything = "hello"; //문자도 담을 수 있다.
+
+    //viod : 아무런 값을 리턴하지 않을 때 사용
+    function print(): void {
+        console.log("");
+    }
+
+    let unusable: void = undefined; //💩 이렇게는 쓰지않는다.
+
+    //never : 리턴하지않음
+    function throwError(message: string): never {
+        //message -> server(log) : 서버에 에러 메시지 전달할 때는 반환값이 없음
+        throw new Error(message);
+
+        //끝나지 않은 코드
+        while (true) {}
+    }
+
+    //object : 원시타입을 제외한 모든 오브젝트 타입 할당 가능
+    let obj: object;
+    function acceptSomeObject(obj: object) {}
+    acceptSomeObject({ name: "ellie" });
+    acceptSomeObject({ animal: "dog" });
 }
