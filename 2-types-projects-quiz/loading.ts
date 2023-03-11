9f63e7776131a29bf09344aa28f022acce5d96fe
park songhee <songhee6392@gmail.com>
2023-03-11 23:42:29 +0900

    로딩 상태 표시

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -1,26 +1,39 @@
 {
-  /**
-   * Print Loading State
-   */
-  type LoadingState = {
-    state: 'loading';
-  };
-
-  type SuccessState = {
-    state: 'success';
-    response: {
-      body: string;
+    /**
+     * Print Loading State
+     */
+    type LoadingState = {
+        state: "loading";
     };
-  };
 
-  type FailState = {
-    state: 'fail';
-    reason: string;
-  };
+    type SuccessState = {
+        state: "success";
+        response: {
+            body: string;
+        };
+    };
 
-  type ResourceLoadState = LoadingState | SuccessState | FailState;
+    type FailState = {
+        state: "fail";
+        reason: string;
+    };
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+    type ResourceLoadState = LoadingState | SuccessState | FailState;
+
+    function printLoginState(state: ResourceLoadState) {
+        switch (state.state) {
+            case "loading":
+                console.log(`👀 ${state.state}...`);
+                break;
+            case "success":
+                console.log(`😃 ${state.response.body}...`);
+                break;
+            case "fail":
+                console.log(`😱 ${state.reason}...`);
+                break;
+        }
+    }
+    printLoginState({ state: "loading" }); // 👀 loading...
+    printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+    printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
 }
