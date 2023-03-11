8ebde69ed75b9ab13af565ac7fe29b805e850b1a
park songhee <songhee6392@gmail.com>
2023-03-11 21:29:39 +0900

    좌표 게임

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,49 @@
 /**
  * Let's make a game 🕹
  */
+type Position = {
+    x: number;
+    y: number;
+};
+
+let position: Position = {
+    x: 0,
+    y: 0,
+};
+
+type Commend = "up" | "down" | "left" | "right";
+function move(commend: Commend): Position {
+    switch (commend) {
+        case "up":
+            position.y += 1;
+            break;
+        case "down":
+            position.y -= 1;
+            break;
+        case "left":
+            position.x -= 1;
+            break;
+        case "right":
+            position.x += 1;
+            break;
+        default:
+            throw new Error(`없는 방향 ${commend}`);
+            break;
+    }
+
+    return position;
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+
+move("right");
 console.log(position); // { x: 0, y: 0}
